3c2a4ff9b1a9345b0bc859084342287363c4f95d
babingthon <babingthon.bandeira@gmail.com>
2024-02-07 20:13:06 -0300

    conclusão do exercicio de exceptions

--- Exercicio1/obj/Debug/net7.0/Exercicio1.AssemblyInfo.cs
+++ Exercicio1/obj/Debug/net7.0/Exercicio1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13457fac77ab30ec8a7374628daf28894b613af5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23c0e5ab0efcb51faa3bf1e7fd056dd06c42919f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Exercio_Exceptions/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using Exercio_Exceptions.Entities;
+using Exercio_Exceptions.Entities.Exceptions;
+
+try
+{
+    Console.WriteLine("Enter account data: ");
+    Console.Write("Number: ");
+    int number = int.Parse(Console.ReadLine());
+    Console.Write("Holder: ");
+    string holder = Console.ReadLine();
+    Console.Write("Initial balance: ");
+    double initialBalance = double.Parse(Console.ReadLine());
+    Console.Write("Withdraw limit: ");
+    double withdrawLimit = double.Parse(Console.ReadLine());
+
+    var account = new Account(number, holder, initialBalance, withdrawLimit);
+
+    Console.Write("Enter amount for withdraw: ");
+    double amount = double.Parse(Console.ReadLine());
+    account.Withdraw(amount);
+
+    Console.Write($"New balance: {account.Balance}");
+}
+catch (DomainException e)
+{
+    Console.BackgroundColor = ConsoleColor.Red;
+    Console.Write("Withdraw error: " + e.Message);
+}
+
+
+
